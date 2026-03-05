f371e533d4a1352f31eb3d670caae14424804ec7
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-03-05 22:05:05 +0530

    resolve stash conflict and update layout

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -2,17 +2,37 @@
 import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchProducts } from "@/lib/features/product/productSlice";
+import { fetchProducts} from "@/lib/features/product/productSlice"
+import {useUser, useAuth} from "@clerk/nextjs"
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
 
 export default function PublicLayout({ children }) {
 
     const dispatch = useDispatch()
+    const {user} = useUser()
+    const {getToken} = useAuth()
+
+    const {cartItems} = useSelector((state) => state.cart)
 
     useEffect(() => {
-        dispatch(fetchProducts({}))
-    }, [])
+        dispatch(fetchProducts())
+    },[])
+
+    useEffect(() => {
+        if(user){
+            dispatch(fetchCart({getToken}))
+        }
+    },[user])
+
+    useEffect(() => {
+        if(user){
+            dispatch(uploadCart({getToken}))
+        }
+    },[cartItems])
+
+
 
     return (
         <>
